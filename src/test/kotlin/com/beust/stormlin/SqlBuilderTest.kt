a98b293f90969350859562dcfc2cbe08c7f09aab
Cedric Beust <cedric@beust.com>
2017-01-23 16:12:21 -0800

    Refactor.

--- src/test/kotlin/com/beust/stormlin/SqlBuilderTest.kt
+++ src/test/kotlin/com/beust/stormlin/SqlBuilderTest.kt
@@ -3,21 +3,21 @@ package com.beust.stormlin
 import org.assertj.core.api.Assertions
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
-import java.sql.Connection
-import java.sql.DriverManager
 
 class SqlBuilderTest {
-    val conn: Connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/perry", "root", "aaaa")
-    val orm = Orm(conn)
+//    val conn: Connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/perry", "root", "aaaa")
+//    val orm = Orm(conn)
 
     @DataProvider
     fun dpSql(): Array<Array<Any>> {
+        fun b() = SqlBuilder<Any>()
+
         return arrayOf(
-            arrayOf(SqlBuilder<Any>().select().from("cycles"), "SELECT * FROM cycles"),
-            arrayOf(SqlBuilder<Any>().select("a", "b").from("cycles"), "SELECT a,b FROM cycles"),
-            arrayOf(SqlBuilder<Any>().select().from("cycles").where("a").eq(2000),
+            arrayOf(b().select().from("cycles"), "SELECT * FROM cycles"),
+            arrayOf(b().select("a", "b").from("cycles"), "SELECT a,b FROM cycles"),
+            arrayOf(b().select().from("cycles").where("a").eq(2000),
                     "SELECT * FROM cycles WHERE a = 2000"),
-            arrayOf(SqlBuilder<Any>().select().from("cycles").where("a").eq("foo"),
+            arrayOf(b().select().from("cycles").where("a").eq("foo"),
                     "SELECT * FROM cycles WHERE a = 'foo'")
         )
     }

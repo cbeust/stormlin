90fd828b17e09cf4604a159a6eda3e63587bca35
Cedric Beust <cedric@beust.com>
2017-01-24 22:17:50 -0800

    Real test.

--- src/test/kotlin/com/beust/stormlin/DbTest.kt
+++ src/test/kotlin/com/beust/stormlin/DbTest.kt
@@ -1,12 +1,15 @@
 package com.beust.stormlin
 
+import org.assertj.core.api.Assertions
+import org.testng.annotations.Test
 import java.sql.DriverManager
 
 class DbTest {
     val urlMemory = "jdbc:sqlite::memory:"
     val connection = DriverManager.getConnection(urlMemory)
 
-    fun run() {
+    @Test
+    fun simpleInMemoryTest() {
         listOf(
             "create table test(id integer primary key, text varchar(80))",
             "insert into test values(1, \"foo\")",
@@ -14,17 +17,14 @@ class DbTest {
         ).forEach {
             val rs = connection.createStatement().execute(it)
         }
-        val rs = connection.createStatement().executeQuery("select * from test where id = 2")
-        while (rs.next()) {
-            val result = rs.getString(2)
-            println("Read: $result")
+
+        fun getId(id: Int) : String {
+            val rs = connection.createStatement().executeQuery("select * from test where id = $id")
+            if (rs.next()) return rs.getString(2)
+            else throw AssertionError("Couldn't find id $id")
         }
 
+        Assertions.assertThat(getId(1)).isEqualTo("foo")
+        Assertions.assertThat(getId(2)).isEqualTo("bar")
     }
-
-}
-
-fun main(argv: Array<String>) {
-    DbTest().run()
-    println("")
 }
